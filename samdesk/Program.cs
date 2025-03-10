2646619863bcea1968b73c5204f2ea8cf669fd88
cheshir78 <cheshir78@gmail.com>
2025-03-10 16:38:50 -0600

    Final test

--- samdesk/Program.cs
+++ samdesk/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SamDesk
 {
@@ -10,7 +8,10 @@ namespace SamDesk
     {
         static void Main(string[] args)
         {
-
+            ShortDistance test = new ShortDistance();
+            Console.WriteLine(string.Format("Result is {0}",test.SolveLists(new List<int>() { 4, 3, 5, 3, 9, 3 }, new List<int>() { 3, 4, 2, 1, 3, 3 })));
+            Console.WriteLine("Real input date is in SamDeskTests\\input.txt");
+            Console.WriteLine("Test method SolveListsFromFileTest() is in SamDeskTests\\ShortDistanceTests.cs");
         }
     }
 }
