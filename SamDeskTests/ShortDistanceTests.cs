12a7be664422a70d5d45aefabd43582eae7452b0
cheshir78 <cheshir78@gmail.com>
2025-03-10 15:15:28 -0600

    Add project files.

---
+++ SamDeskTests/ShortDistanceTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SamDesk.Tests
+{
+    [TestClass()]
+    public class ShortDistanceTests
+    {
+        [TestMethod()]
+        public void SolveListsTest()
+        {
+            var listRight = new List<int>() { 4, 3, 5, 3, 9, 3 }; 
+            var listLeft = new List<int>() { 3, 4, 2, 1, 3, 3 };
+            var test = new ShortDistance();
+            Assert.AreEqual(11, test.SolveLists(listLeft, listRight));
+        }
+
+        [TestMethod()]
+        public void SolveListsFromFileTest()
+        {
+            var listRight = new List<int>();
+            var listLeft = new List<int>();
+            string[] stringSeparators = new string[] { "   " };
+            var lines = File.ReadLines("..\\..\\input.txt");
+            foreach (var line in lines)
+            {
+                
+                listLeft.Add(int.Parse(line.Split(stringSeparators, StringSplitOptions.None)[0]));
+                listRight.Add(int.Parse(line.Split(stringSeparators, StringSplitOptions.None)[1]));
+            }
+            var test = new ShortDistance();
+            Assert.AreEqual(2285373, test.SolveLists(listLeft, listRight));
+        }
+    }
+
+}
---
+++ samdesk/ShortDistance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SamDesk
+{
+    public class ShortDistance
+    {
+        public int SolveLists(List<int> leftList, List<int> rightList) {
+            int res = 0;
+            leftList.Sort();
+            rightList.Sort();
+            for (int i = 0; i < leftList.Count; i++)
+            {
+                res += Math.Abs(leftList[i] - rightList[i]);
+            }
+            return res;
+        }
+    }
+}
